626f0e1d0fa9a4ffe6e9d0e1fcd49f85d1cb6bbf
Geremias Santos <Geremias99999@gmail.com>
2022-06-15 13:14:07 -0300

    AverageSalary Exercise C#

---
+++ AverageSalaryApp/Class/People.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageSalaryApp.Class
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public People()
+        {
+        }
+
+        public People(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
---
+++ AverageSalaryApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using AverageSalaryApp.Class;
+using System;
+using System.Globalization;
+
+namespace AverageSalaryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            People p = new People();
+            People p1 = new People();
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            p.Name = Console.ReadLine();
+            Console.WriteLine("Salario: ");
+            p.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do Segundo funcionário:");
+            Console.WriteLine("Nome: ");
+            p1.Name = Console.ReadLine();
+            Console.WriteLine("Salario: ");
+            p1.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = p.Salary + p1.Salary / 2;
+
+            Console.WriteLine("Salário Médio " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+            
+
+        }
+    }
+}
